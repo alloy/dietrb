b783c6cad5a87c9bc2b3c215687107dcaad42f9c
Eloy Duran <eloy.de.enige@gmail.com>
2010-05-30 15:50:51 +0200

    Release version 0.4.4, the second EuRuKo edition yo!

--- lib/irb/version.rb
+++ lib/irb/version.rb
@@ -8,7 +8,7 @@ module IRB
   module VERSION #:nodoc:
     MAJOR = 0
     MINOR = 4
-    TINY  = 3
+    TINY  = 4
     
     STRING = [MAJOR, MINOR, TINY].join('.')
     DESCRIPTION = "#{STRING} (DietRB)"
